d45c017b8a3e17daa6f5d30c5daa91d7c5ea6628
Leo Feng <thcheetah777@gmail.com>
2024-01-07 17:56:13 +0100

    🔥 second fire attack - donw

--- bosses/fire_boss.gd
+++ bosses/fire_boss.gd
@@ -10,14 +10,17 @@ enum STATE {
 @export var animation_speed = 2
 @export var fireball_positions: Array[Marker2D]
 @export var fireball_speed = 90
+@export var bullet_ring_num = 20
+@export var bullet_ring_speed = 100
 
 @onready var eye := $Eye
 
 var state = STATE.FIREBALLS
 var fireball_scene = preload("res://enemy-bullets/fireball.tscn")
+var bullet_scene = preload("res://enemy-bullets/fire_bullet.tscn")
 
 func _ready() -> void:
-	fireballs_state()
+	choose_random_state()
 
 func _process(delta: float) -> void:
 	var direction = (Globals.player.position - global_position).normalized()
@@ -28,16 +31,32 @@ func _process(delta: float) -> void:
 func fireballs_state():
 	for i in range(2):
 		var random_position = fireball_positions.pick_random().position
-		await move(random_position, 2.0)
+		await move(random_position, 1.5)
 		await Globals.wait(0.5)
 
 		for x in range(3):
 			fire_fireball()
 			await Globals.wait(0.5)
 
-	fireballs_state()
+	# fireballs_state()
+	choose_random_state()
 
 func bullet_ring_state():
+	await move(Vector2(352, 192), 2.0)
+
+	var offset = 0
+	for i in range(2):
+		for x in range(bullet_ring_num):
+			var bullet = bullet_scene.instantiate() as EnemyBullet
+			bullet.position = position
+			bullet.rotation_degrees = 360.0 / bullet_ring_num * x + offset
+			bullet.speed = bullet_ring_speed
+			Globals.world.add_child(bullet)
+
+		offset += 9
+		await Globals.wait(0.4)
+
+	# bullet_ring_state()
 	choose_random_state()
 
 func laser_extrude_state():
@@ -63,3 +82,8 @@ func move(target_position: Vector2, duration: float):
 
 func choose_random_state():
 	state = STATE.keys()[randi() % STATE.size()]
+	match state:
+		"FIREBALLS": fireballs_state()
+		"BULLET_RING": bullet_ring_state()
+		"LASER_EXTRUDE": fireballs_state()
+		"LASER_FOLLOW": bullet_ring_state()
