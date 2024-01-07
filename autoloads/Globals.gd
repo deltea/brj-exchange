a35328769c404accad05bcd70f3fb4c678df14f9
Leo Feng <thcheetah777@gmail.com>
2024-01-07 16:25:16 +0100

    👾 basic boss state manager

--- autoloads/Globals.gd
+++ autoloads/Globals.gd
@@ -8,3 +8,6 @@ var time = 0.0
 
 func _process(delta: float) -> void:
 	time += delta
+
+func wait(duration: float):
+	await get_tree().create_timer(duration, false, false, true).timeout
--- bosses/fire_boss.gd
+++ bosses/fire_boss.gd
@@ -1,14 +1,32 @@
 extends Boss
 
 enum STATE {
-
+	FIREBALLS,
+	BULLET_RING,
+	LASER_EXTRUDE,
+	LASER_FOLLOW,
 }
 
 @onready var eye := $Eye
 
+var state = STATE.FIREBALLS
+
 func _process(_delta: float) -> void:
 	var direction = (Globals.player.position - global_position).normalized()
 	eye.position = direction * 5
+	choose_random_state()
 
-func _physics_process(delta: float) -> void:
-	position = position.move_toward(Globals.player.position, 30 * delta)
+func fireballs_state():
+	choose_random_state()
+
+func bullet_ring_state():
+	choose_random_state()
+
+func laser_extrude_state():
+	choose_random_state()
+
+func laser_follow_state():
+	choose_random_state()
+
+func choose_random_state():
+	state = STATE.keys()[randi() % STATE.size()]
