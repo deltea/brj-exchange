4888013457cdf515f694986742bbb7ecc242818a
Leo Feng <thcheetah777@gmail.com>
2024-01-09 21:16:17 +0100

    🃏 better cards

--- exchange.gd
+++ exchange.gd
@@ -10,7 +10,7 @@ var card_scene = preload("res://ui/card.tscn")
 func _ready() -> void:
 	var upgrade_num = 5
 	var random_upgrades = UpgradeManager.get_random_upgrades(upgrade_num)
-	var tween = get_tree().create_tween().set_trans(Tween.TRANS_CUBIC).set_parallel()
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_EXPO).set_parallel()
 	for i in range(upgrade_num):
 		var card = card_scene.instantiate() as Card
 		card.position = Vector2(480.0 / 2.0 - (52.0 / 2), 0 - 68)
@@ -20,8 +20,7 @@ func _ready() -> void:
 		#! Add the card tilting and y offset DYNAMICALLY
 		var card_x = 240 + i * card_separation - (upgrade_num - 1) * card_separation / 2.0
 		var card_y = 64 + abs(i - 2) * card_y_offset
-		var target_rotation = i * card_tilt - 2 * card_tilt
+		card.tilted_rotation = i * card_tilt - 2 * card_tilt
 		tween.chain().tween_property(card, "position", Vector2(card_x, card_y), tween_speed)
-		tween.tween_property(card, "rotation_degrees", target_rotation, tween_speed)
 
 		add_child(card)
--- ui/card.gd
+++ ui/card.gd
@@ -9,17 +9,23 @@ class_name Card
 var upgrade: UpgradeResource
 var animation_offset = 0
 var hovering = false
+var target_rotation = 0
+var tilted_rotation: float
 
 func _ready() -> void:
 	sprite.texture = upgrade.texture
+	target_rotation = tilted_rotation
 
 func _process(delta: float) -> void:
 	sprite.position.y = move_toward(sprite.position.y, hover_y if hovering else 0, animation_speed * delta)
+	sprite.rotation_degrees = move_toward(sprite.rotation_degrees, target_rotation, 150 * delta)
 	if not hovering:
 		sprite.offset = Vector2(0, sin(Globals.time * 2 + (animation_offset * 7)) * 1.4)
 
 func _on_mouse_entered() -> void:
 	hovering = true
+	target_rotation = 0
 
 func _on_mouse_exited() -> void:
 	hovering = false
+	target_rotation = tilted_rotation
