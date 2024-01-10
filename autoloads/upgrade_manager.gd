58ce59f6fa8c5611669666ae81c5d3c8e1091d84
Leo Feng <thcheetah777@gmail.com>
2024-01-10 20:45:26 +0100

    🎲 some stuff

--- autoloads/upgrade_manager.gd
+++ autoloads/upgrade_manager.gd
@@ -1,4 +1,11 @@
 extends Node
+class_name Upgrades
+
+enum UPGRADES {
+	LIFE,
+	SPEED,
+	DAMAGE
+}
 
 @export var upgrades: Array[UpgradeResource] = []
 
@@ -9,3 +16,16 @@ func get_random_upgrades(amount: int) -> Array[UpgradeResource]:
 	for i in range(amount):
 		result.append(upgrades.pick_random())
 	return result
+
+func activate_upgrade(upgrade: UPGRADES):
+	var method = UPGRADES.keys()[upgrade].to_lower() + "_upgrade"
+	if has_method(method): call(method)
+
+func life_upgrade():
+	pass
+
+func speed_upgrade():
+	pass
+
+func damage_upgrade():
+	pass
--- upgrade_resource.gd
+++ upgrade_resource.gd
@@ -5,3 +5,4 @@ class_name UpgradeResource
 @export var description = ""
 @export_range(1, 3) var exchange_cost = 1
 @export var texture: Texture2D
+@export var method: Upgrades.UPGRADES = Upgrades.UPGRADES.LIFE
