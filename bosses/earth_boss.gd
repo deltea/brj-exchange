a631668f7434dc53cd1e40048c30562eebd4e962
Leo Feng <thcheetah777@gmail.com>
2024-01-14 17:35:04 +0100

    🐍 snake

--- bosses/earth_boss.gd
+++ bosses/earth_boss.gd
@@ -1,2 +1,12 @@
 extends Boss
 class_name EarthBoss
+
+@export var move_speed = 50
+
+var normal = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	var prev_position = position
+	position = position.move_toward(Globals.player.position, move_speed * delta)
+	normal = (position - prev_position).normalized()
+	rotation = normal.angle() + PI / 2
