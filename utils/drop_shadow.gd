a031455dae9a96bf4afee0bd86bcecb82d78782d
Leo Feng <thcheetah777@gmail.com>
2024-01-14 15:37:40 +0100

    🌍 earth boss background

--- utils/drop_shadow.gd
+++ utils/drop_shadow.gd
@@ -2,7 +2,6 @@ extends Sprite2D
 class_name DropShadow
 
 @export var shadow_offset = Vector2(0, 8)
-@export var shadow_color: Color = Color.DARK_GRAY
 @export var scale_multiplier = Vector2.ONE
 @export var ordering = -20
 
@@ -14,7 +13,7 @@ func _ready():
 	parent_sprite = get_parent() as Sprite2D
 	texture = parent_sprite.texture
 	z_index = ordering
-	material.set_shader_parameter("color", shadow_color)
+	material.set_shader_parameter("color", Globals.world.shadow_color)
 
 func _process(_delta: float) -> void:
 	if not get_parent(): return
--- world.gd
+++ world.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name World
 
 @export var background_color: Color = Color.BLACK
+@export var shadow_color: Color = Color.BLACK
 
 func _enter_tree() -> void:
 	Globals.world = self
