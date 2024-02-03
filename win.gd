f7ddbd4317bcecb67e006ab887d19d820b595fa3
Leo Feng <thcheetah777@gmail.com>
2024-02-03 12:14:56 -0600

    🥇 more winning

--- win.gd
+++ win.gd
@@ -12,7 +12,8 @@ extends CanvasLayer
 @onready var health_penalty := $HealthPenalty
 @onready var total_time_label := $TotalTime
 @onready var dotted_line := $DottedLine
-@onready var cards := $Cards
+@onready var cards_row := $Cards
+@onready var cards_owned_text := $CardsOwnedText
 
 var card_scene = preload("res://ui/card.tscn")
 
@@ -24,17 +25,18 @@ func _ready() -> void:
 	health_penalty.visible = false
 	total_time_label.visible = false
 	dotted_line.region_rect = Rect2(0, 0, 0, 4)
-	cards.visible = false
+	cards_row.visible = false
+	cards_owned_text.visible = false
 
 	await Globals.wait(1.0)
 
 	var tween = get_tree().create_tween()
 
 	tween.tween_callback(func(): times_column.visible = true)
-	tween.tween_method(func(value): set_boss_time(value, fire_time), 0, snapped(Scoring.boss_times[0], 0.01), score_count_duration)
-	tween.tween_method(func(value): set_boss_time(value, water_time), 0, snapped(Scoring.boss_times[1], 0.01), score_count_duration)
-	tween.tween_method(func(value): set_boss_time(value, earth_time), 0, snapped(Scoring.boss_times[2], 0.01), score_count_duration)
-	tween.tween_method(func(value): set_boss_time(value, air_time), 0, snapped(Scoring.boss_times[3], 0.01), score_count_duration)
+	tween.tween_method(func(value): set_boss_time(value, fire_time), 0.0, Scoring.boss_times[0], score_count_duration)
+	tween.tween_method(func(value): set_boss_time(value, water_time), 0.0, Scoring.boss_times[1], score_count_duration)
+	tween.tween_method(func(value): set_boss_time(value, earth_time), 0.0, Scoring.boss_times[2], score_count_duration)
+	tween.tween_method(func(value): set_boss_time(value, air_time), 0.0, Scoring.boss_times[3], score_count_duration)
 
 	var total_boss_time = 0
 	for i in range(4):
@@ -42,30 +44,31 @@ func _ready() -> void:
 	var total_time = total_boss_time + Scoring.total_health_lost
 
 	tween.tween_callback(func(): boss_total_time.visible = true)
-	tween.tween_method(set_boss_total_time, 0, total_boss_time, score_count_duration)
+	tween.tween_method(set_boss_total_time, 0.0, total_boss_time, score_count_duration)
 	tween.tween_callback(func(): health_penalty.visible = true)
-	tween.tween_method(set_health_penalty, 0, Scoring.total_health_lost, score_count_duration)
+	tween.tween_method(set_health_penalty, 0.0, Scoring.total_health_lost, score_count_duration)
 	tween.tween_property(dotted_line, "region_rect", Rect2(0, 0, 150, 4), score_count_duration)
 	tween.tween_callback(func(): total_time_label.visible = true)
-	tween.tween_method(set_total_time, 0, total_time, score_count_duration)
+	tween.tween_method(set_total_time, 0.0, total_time, score_count_duration)
 
 	for upgrade in UpgradeManager.current_upgrades:
 		var card = card_scene.instantiate() as Card
 		card.upgrade = upgrade
-		cards.add_child(card)
-	tween.tween_callback(func(): cards.visible = true)
+		cards_row.add_child(card)
+	tween.tween_callback(func(): cards_row.visible = true)
+	tween.tween_callback(func(): cards_owned_text.visible = true)
 
 func set_boss_time(value: float, label: Label):
-	label.text = str(value) + "s"
+	label.text = str(snapped(value, 0.01)) + "s"
 
 func set_boss_total_time(value: float):
-	boss_total_time.text = "Boss Total Time: +%ss" % str(value)
+	boss_total_time.text = "Boss Total Time: +%ss" % str(snapped(value, 0.01))
 
 func set_health_penalty(value: float):
 	health_penalty.text = "Health Penalty: +%ss" % str(value)
 
 func set_total_time(value: float):
-	total_time_label.text = "Total Time: %ss" % str(value)
+	total_time_label.text = "Total Time: %ss" % str(snapped(value, 0.01))
 
 func _on_main_menu_button_pressed() -> void:
 	SceneManager.change_scene(SceneManager.main_menu_scene)
